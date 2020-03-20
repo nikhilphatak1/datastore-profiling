c955e0a3de8ce48224b152303dcac201c322f633
Nikhil Phatak <nikhil.phatak@lookout.com>
2020-03-20 00:22:58 -0400

    add dynamodb config with pynamo and docker

---
+++ dynamodb/dynamo.py
@@ -0,0 +1,36 @@
+import dotenv
+import pynamodb.attributes as attrs
+import pynamodb.models as models
+
+
+class Thingy(models.Model):
+    class Meta:
+        table_name = 'Thingy'
+        host = None
+
+    id = attrs.NumberAttribute(hash_key=True)
+    name = attrs.UnicodeAttribute()
+
+    def __repr__(self):
+        return "<Thingy {} name:{}>".format(self.id, self.name)
+
+
+def main(cfg):
+    dotenv.load_dotenv()
+
+    if Thingy.exists():
+        Thingy.delete_table()
+    Thingy.create_table(read_capacity_units=1, write_capacity_units=1, wait=True)
+
+    thang = Thingy(id=0, name="dodo")
+    thang.save()
+
+    stored_thingy = Thingy.get(0)
+    print(stored_thingy)
+
+    for t in Thingy.scan(Thingy.name == "dodo"):
+        print(t)
+
+
+if __name__ == "__main__":
+    main()
